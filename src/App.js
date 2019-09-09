b05efdecb20a59a4abd1a717467cef4521530f7c
Filipe Lago Peres <filipelperes@gmail.com>
2019-09-09 16:08:36 -0300

    Searching in the bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 
 import React from 'react';
+import { Provider } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
+import store from './store'
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
 import VideoPlayer from './components/VideoPlayer';
 
 function App() {
   return (
-    <div className="container">
-      <SearchBar />
-      <VideoList />
-      <VideoPlayer />
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <SearchBar />
+        <VideoList />
+        <VideoPlayer />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,17 @@ import {
     Segment,
     Input
 } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { dispatch } from 'rxjs/internal/observable/range';
+
+import { buscaVideo } from './../store/actions/busca-video'
 
 //const SearchBar = props => {
 class SearchBar extends Component {
     pesquisaTermo = e => {
         if(e.keyCode === 13) {
             const termo = e.target.value;
+            this.props.buscaVideo(termo);
         }
     }
 
@@ -23,4 +28,10 @@ class SearchBar extends Component {
     }
 }
 
-export default SearchBar;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buscaVideo: termo => dispatch(buscaVideo(termo))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
